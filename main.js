a30a9aaa6e7af834ccb1029b9a903e15d958fe83
richard.zevel <richard.zevel@actumdigital.com>
2022-04-11 11:00:16 +0200

    resolved issue between bootstrap 4 and 5, switching to modals instead of collapse bcs content.

--- main.js
+++ main.js
@@ -26,6 +26,9 @@ fs.readFile(jsonPath, (err,data) => {
     htmlContent += violationsHtml
     // htmlContent += incompleteHtml
     // htmlContent += inaplicableHtml
+    htmlContent += `\n<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"></script>`
     htmlContent+= "\n</body>\n</html>"    
     
     // Write final HTML into file.
