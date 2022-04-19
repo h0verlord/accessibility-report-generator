4bdb4d9af836c0a009462a50c71bdf9117b984f6
richard.zevel <richard.zevel@actumdigital.com>
2022-04-19 13:43:24 +0200

    played around with the structure a bit.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
 import Parser from './parserClass.js'
 
-const jsonPath = 'reports/json/report.json'
+const jsonPath = './reports/json/report.json'
 
 // read json file
 fs.readFile(jsonPath, (err, data) => {
@@ -10,7 +10,7 @@ fs.readFile(jsonPath, (err, data) => {
   const date = Date.now()
 
   // read html from template
-  const htmlContent = fs.readFileSync('report-template.html').toString()
+  const htmlContent = fs.readFileSync('./reports/html/report-template.html').toString()
   const parser = new Parser(objectData, htmlContent)
 
   // Parse Json, generate some elements and append to htmlContent
@@ -20,3 +20,4 @@ fs.readFile(jsonPath, (err, data) => {
     if (err) throw err
   })
 })
+
